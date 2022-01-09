ce3a6ebdf0557aab3b0a5fd7e78c19be06836e83
samini1 <92944248+samini1@users.noreply.github.com>
2022-01-09 13:09:54 -0600

    reorganizing files into new folder prompts for readme generator created inquirer and fs modules added generate function module added from folder

---
+++ Develop/index.js
@@ -0,0 +1,135 @@
+// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    {
+    type: 'input',
+    name: 'title',
+    message: 'Enter project title.',
+    validate: titleInput => {
+        if (titleInput) {
+            return true;
+        } else {
+            console.log('Please enter a valid response');
+            return false;
+        }
+    }
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project',
+        validate: desInput => {
+            if (desInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'install',
+        message: 'Enter installation information',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information',
+        validate: useInput => {
+            if (useInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    }, 
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Enter guidelines for contributing',
+        validate: contInput => {
+            if (contInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'Enter test instructions',
+        validate: testInput => {
+            if (testInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    },    
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application.',
+        choices: ['MIT','GPL', 'Apache', 'Boost', 'Unlicense']
+    },
+    {
+        type: 'input',
+        name: 'gitub',
+        message: 'Enter your GitHub username',
+        validate: gitInput => {
+            if (gitInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address',
+        validate: emailInput => {
+            if (emailInput) {
+                return true;
+            } else {
+                console.log('Please enter a valid response');
+                return false;
+            }
+        }
+    }
+       
+];
+inquirer
+    .prompt(questions)
+    .then((answers) => { 
+        console.log(answers)
+
+    })
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
