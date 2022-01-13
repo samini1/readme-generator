ce6e0a0f4edfd0b34358269c924ea4872ce914b4
samini1 <92944248+samini1@users.noreply.github.com>
2022-01-13 15:03:20 -0600

    started laying out content that needs to be generated in readme file testing file generation

--- Develop/index.js
+++ Develop/index.js
@@ -117,19 +117,30 @@ const questions = [
     }
        
 ];
-inquirer
-    .prompt(questions)
-    .then((answers) => { 
-        console.log(answers)
 
-    })
 
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(README.md, generateMarkdown(data), err => {
+        if (err) {
+            return console.log(err);
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => { 
+        console.log('Using these responses to generate a README: '); 
+        console.log(answers);
+
+    })
+
+    writeToFile();
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (data.license = '') {
+    return ''
+  } else {
+    return `[![License](https://img.shields.io/badge/${image}/${url}`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -12,7 +18,42 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+
+return `
+# Your Project Title
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+
+## Installation
+
+${data.install}
+
+## Usage
+
+${data.usage}
+
+## License
+
+${data.license}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+Further questions about this project may be directed to:
+${data.email}
+${data.github}
 
 `;
 }
